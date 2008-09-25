2941f2e338fb0db304ea47a1cf797587b3351eed
morillas <morillas@02d82bf8-8a0b-11dd-8a42-c3ad82d480ce>
2008-09-25 12:08:45 +0000

    Ejercicios circulo, cuadrilatero y cuadrado

---
+++ trunk/primeros/perimetro_cuadrado.py
@@ -0,0 +1,21 @@
+# -*- coding: cp1252 -*-
+
+"""
+Escribe un programa perimetro_cuadrado.py, que pide al usuario
+el valor del lado de un cuadrado y muestra su perímetro.
+
+AUTOR
+VERSION
+"""
+
+# pedir datos
+lado = input("Introduce el lado: ")
+
+# calcular resultado
+perim = lado * 4
+
+# mostrar el resultado
+print "El perimetro de un cuadrado de lado", lado, "es", perim
+
+# fin
+raw_input("Pulsa intro para terminar")
