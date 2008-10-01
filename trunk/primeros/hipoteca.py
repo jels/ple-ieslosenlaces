11822cde20af4d6b2516e822c49415abc75b876e
morillas <morillas@02d82bf8-8a0b-11dd-8a42-c3ad82d480ce>
2008-10-01 11:41:57 +0000

    Modificaciones hipoteca

--- trunk/primeros/hipoteca.py
+++ trunk/primeros/hipoteca.py
@@ -16,14 +16,17 @@ AUTOR: PABLO MARTIN
 VERSION: 1.0
 '''
 
+from decimal import *
+
 #Introduccion al programa
 print ' Hola, con este programa calcularemos la cuota mensual de una hipoteca '
 print '*'*80
 
 #Entrada de datos
-capital = float (input (' Introduzca la cantidad que va a pedir: '))
-anos = input (' Introduzca los años en los que desea pagar el prestamo: ')
-interes = float (input (' Introduzca el T.A.E que le aplica el banco: '))
+capital = Decimal (raw_input (' Introduzca la cantidad que va a pedir: '))
+anos = Decimal(raw_input (' Introduzca los años en los que desea pagar el prestamo: '))
+interes = Decimal (raw_input (' Introduzca el T.A.E que le aplica el banco: '))
+
 
 #Operaciones necesarias
 plazos = anos * 12
@@ -39,6 +42,29 @@ print ' En una hipoteca de capital: ', capital, ' y con un interes de ', interes
 print ' a pagar en: ', anos, ' años '
 print ' Tendra que pagar ', plazos, ' cuotas mensuales de ', cuota, ' euros '
 print ' Con lo que usted debera pagar un total de: ', hipoteca
+
+print '*' * 80
+
+pagado = 0  # lo que hemos pagado del préstamo hasta ahora (amortización)
+interes_mensual = interes / 12 / 100   # para calcular intereses de cada mes
+
+print "%4s %12s %12s %12s" % ("Mes", "Intereses", "Amortiza", "Pendiente")
+for mes in range(plazos):
+    # datos que voy a mostrar 
+    intereses_mes_actual = (capital-pagado) * interes_mensual
+    amortiza_mes_actual = cuota - intereses_mes_actual
+    # cálculos
+    pagado = pagado + amortiza_mes_actual
+    print "%4d %12.2f %12.2f %12.2f" % (mes+1, intereses_mes_actual, \
+                                       amortiza_mes_actual, capital-pagado)
+    
+    
+    
+    
+    
+
+
+
 
 #Fin
 raw_input (' Pulse inro para salir ')
