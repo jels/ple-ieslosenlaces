3e2dc2825f4c97c324357a4e39caacecd81cd1f8
morillas <morillas@02d82bf8-8a0b-11dd-8a42-c3ad82d480ce>
2008-10-02 09:01:42 +0000

    Introducción cadenas caracteres

---
+++ trunk/cadenas/mes.py
@@ -0,0 +1,27 @@
+# -*- encoding: utf-8 -*-
+
+"""
+Este programa pide un número de mes al usuario 
+y escribe nombre del mes abreviado
+Almacenamos los meses en una cadena:
+Mes    Número    Posición
+-------------------------
+Ene      1         0
+Feb      2         3
+Mar      3         6
+...
+
+Las posiciones son múltiplos de 3. Para obtener la posición, 
+le restamos 1 al número de mes y lo multiplicamos por 3
+
+"""
+
+meses = "EneFebMarAbrMayJunJulAgoSepOctNovDic"
+
+num_mes = input("Introduce el número del mes: ")
+
+pos = (num_mes -1) * 3
+
+mes_abrev = meses[pos:pos+3]
+
+print 'El mes número', num_mes, 'es', mes_abrev
---
+++ trunk/cadenas/usuario.py
@@ -0,0 +1,9 @@
+# -*- encoding: utf-8 -*-
+
+# Pide nombre y apellido
+# Construye nombre de usuario: inicial del nombre + apellido
+
+nombre = raw_input("Introduce tu nombre (en minúsculas): ")
+apellido = raw_input("Inrtroduce tu apellido (en minúsculas): ")
+
+print 'Nombre de usuario: ', nombre[0]+apellido
