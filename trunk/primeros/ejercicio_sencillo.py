b5858865964b7da302477e9c70c81918e24305a1
morillas <morillas@02d82bf8-8a0b-11dd-8a42-c3ad82d480ce>
2008-10-07 09:06:55 +0000

    Repaso

---
+++ trunk/primeros/ejercicio_sencillo.py
@@ -0,0 +1,19 @@
+# -*- coding: cp1252 -*-
+"""
+Qué es lo que hace el programa
+Cómo se utiliza
+Tests?
+
+Autor
+"""
+
+print "Primer programa de Python"
+
+# raw_input --> cadenas de caracteres
+nombre = raw_input("Escribe tu nombre: ")
+
+edad = input("Escribe tu edad: ")
+
+print nombre, 'tiene', edad, 'años.'
+
+raw_input("Pulsa intro para terminar")
