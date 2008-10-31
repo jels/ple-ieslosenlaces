b9b9dcdaee015f3e43ee090d1eb42e7ec1bd38dc
morillas <morillas@02d82bf8-8a0b-11dd-8a42-c3ad82d480ce>
2008-10-31 10:52:56 +0000

    dibuja triángulos

---
+++ trunk/graficos/triangulo.py
@@ -0,0 +1,21 @@
+# -*- encoding: utf-8 -*-
+"""
+Dibuja un polígono en la pantalla
+y lo rellena de color
+"""
+
+from graphics import *
+
+ventana = GraphWin("Polígono")
+Text(Point(25, 15), 'Color:').draw(ventana)
+color = Entry(Point(120, 15), 10)
+color.draw(ventana)
+while True:
+    p1 = ventana.getMouse()
+    p2 = ventana.getMouse()
+    p3 = ventana.getMouse()
+    tri = Polygon(p1,p2, p3)
+    tri.setFill(color.getText())
+    tri.draw(ventana)
+ventana.getMouse()
+
