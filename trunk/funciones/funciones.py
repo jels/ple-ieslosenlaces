68a4fd26500a6b7f263ab2f44ee81ff022511dce
morillas <morillas@02d82bf8-8a0b-11dd-8a42-c3ad82d480ce>
2008-11-04 10:14:02 +0000

    funciones + graphics

--- trunk/funciones/funciones.py
+++ trunk/funciones/funciones.py
@@ -1,5 +1,19 @@
-def cuadrado(n):
-    return n*n
+# -*- encoding: utf-8 -*-
 
-def cubo(n):
-    return n*n*n
+"""
+Funciones auxiliares círculo y circunferencia
+"""
+
+from math import pi
+
+def area(radio):
+    """
+    area(radio) --> devuelve área
+    """
+    return pi * radio * radio
+
+def circunferencia(radio):
+    """
+    circunferencia(radio) --> devuelve longitud circunf.
+    """
+    return pi * 2 * radio
