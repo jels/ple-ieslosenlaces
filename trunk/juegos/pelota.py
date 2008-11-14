96c17208aeaf3fe02c3530b79952825ae553667c
morillas <morillas@02d82bf8-8a0b-11dd-8a42-c3ad82d480ce>
2008-11-14 10:22:25 +0000

    Intro pygame

---
+++ trunk/juegos/pelota.py
@@ -0,0 +1,37 @@
+import sys, pygame
+
+"""
+Parte de la introducción a pygame de 
+http://www.losersjuegos.com.ar/referencia/articulos/pygame_intro/pygame_intro.php
+"""
+
+pygame.init()
+
+size = width, height = 320, 240
+speed = [2, 2]
+black = 0, 0, 0
+
+screen = pygame.display.set_mode(size)
+
+ball = pygame.image.load("pelota.png")
+ballrect = ball.get_rect()
+
+while 1:
+    
+    for event in pygame.event.get():
+    
+        if event.type == pygame.QUIT:
+            sys.exit()
+       
+    ballrect = ballrect.move(speed)
+
+    if ballrect.left < 0 or ballrect.right > width:
+        speed[0] = -speed[0]
+        
+    if ballrect.top < 0 or ballrect.bottom > height:
+        speed[1] = -speed[1]
+
+    screen.fill(black)
+    screen.blit(ball, ballrect)
+    pygame.display.flip()
+
