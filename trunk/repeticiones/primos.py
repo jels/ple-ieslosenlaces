9f11c09828c348d7c76beb78f94271b5d1ad29e3
morillas <morillas@02d82bf8-8a0b-11dd-8a42-c3ad82d480ce>
2008-11-18 11:36:48 +0000

    repeticiones

---
+++ trunk/repeticiones/primos.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+
+
+def es_primo_adrian(numero):
+    """
+    Devuelve verdadero si el número es primo. Si no,
+    devuelve falso.
+    """
+    contador = 2
+    primo = True
+    while (primo) and contador != numero:
+        if numero % contador == 0:
+            primo = False
+        contador = contador +1
+    return primo
+
+def es_primo_pablo(numero):
+    for x in range(2,numero):
+        if numero % x == 0:
+            return False
+    return True
+
+    
+contador = 0
+numero = 2
+while contador < 10:
+    if es_primo_pablo(numero):
+        print numero
+        contador = contador +1
+    numero = numero + 1
+        
