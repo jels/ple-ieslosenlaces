8f8f030000d0e20a781c31c3362909b2b7c4af1c
morillas <morillas@02d82bf8-8a0b-11dd-8a42-c3ad82d480ce>
2009-09-24 09:27:39 +0000

    24 de septiembre

---
+++ primeros/media_asignaturas.py
@@ -0,0 +1,10 @@
+# 1. Pedir datos
+nota1 = input("Primera nota: ")
+nota2 = input("Segunda nota: ")
+nota3 = input("Tercera nota: ")
+
+# 2. Calcular media
+media = (nota1 + nota2 + nota3) / 3.
+
+# 3. Mostrar resultado
+print "Nota media:", round(media, 1)
---
+++ turtles/cuadrado.py
@@ -0,0 +1,10 @@
+# -*- coding: cp1252 -*-
+
+# Indicamos que utilice el módulo turtle
+from turtle import *
+
+lt(90)
+for x in range(4):
+    fd(100) # forward = avanzar
+    rt(90)  # right = giro a la derecha
+
