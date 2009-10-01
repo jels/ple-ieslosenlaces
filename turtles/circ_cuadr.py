ff51585b1a300a451e704946ea3b0c5c7e43b6d7
kassy.hc <kassy.hc@02d82bf8-8a0b-11dd-8a42-c3ad82d480ce>
2009-10-01 08:15:40 +0000

    Ejercicios de Casandra

---
+++ turtles/circ_cuadr.py
@@ -0,0 +1,14 @@
+# -*- coding: cp1252 -*-
+
+# Indicamos que utilice el módulo turtle
+from turtle import *
+
+lt(90)
+
+circle(-60)
+
+rt(90)
+
+for x in range(4):
+    fd(120) # forward = avanzar
+    rt(90)  # right   = giro a la derecha
